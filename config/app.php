896b550bd7470ffb0a1c7880b3705ed0b4912a89
Abrar-Abdulwahed <abrar.abdulwahed@gmail.com>
2024-01-02 18:02:59 +0300

    feat: install fortify

--- config/app.php
+++ config/app.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
+use App\Providers\FortifyServiceProvider;
 
 return [
 
@@ -159,6 +160,7 @@ return [
         /*
          * Package Service Providers...
          */
+        FortifyServiceProvider::class,
 
         /*
          * Application Service Providers...
