0282248908fa1d985dc4aa85549bc3a800230880
Abrar-Abdulwahed <abrar.abdulwahed@gmail.com>
2024-01-04 21:51:34 +0300

    refactor: schedule pruning expired tokens

---
+++ app/Console/Commands/PruneExpired.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Laravel\Sanctum\Sanctum;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Laravel\Sanctum\PersonalAccessToken;
+
+class PruneExpired extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sanctum:prune-expired';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete expired sanctum tokens';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $model = Sanctum::$personalAccessTokenModel;
+        $model::where('expires_at', '<', now())->delete();    }
+}
