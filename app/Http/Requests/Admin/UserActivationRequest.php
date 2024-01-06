ae68f14995520ebd43be59c4afdcdb04b367a466
Abrar-Abdulwahed <abrar.abdulwahed@gmail.com>
2024-01-06 21:45:38 +0300

    feat: activate, deactivate non-patient user by super-admin

---
+++ app/Http/Requests/Admin/UserActivationRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use App\Models\User;
+use App\Enums\UserType;
+use Illuminate\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserActivationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'activated' => 'required|boolean'
+        ];
+    }
+
+    public function after(): array
+    {
+        $user = $this->route('user');
+        return [
+            function (Validator $validator) use($user) {
+                if (!$user || $user->type === UserType::PATIENT->value) {
+                    $validator->errors()->add('type', 'The user type must be service provider or supervisor, patient ALWAYS activated!');
+                }
+            },
+        ];
+    }
+}
