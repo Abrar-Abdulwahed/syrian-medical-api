62a8b2f2b5d40d11e955ec9846d7efe295ba1842
Abrar-Abdulwahed <abrar.abdulwahed@gmail.com>
2024-01-08 23:16:24 +0300

    feat: notify service provider about his application

---
+++ app/Notifications/AdminReviewNotificationMail.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class AdminReviewNotificationMail extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public $accept)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $acceptMSG = 'Congratulations! Your application has been accepted, you can update your profile by clicking below button.';
+        $rejectedMSG = 'Unfortunately, Your application has been rejected';
+        $message = $this->accept ? $acceptMSG : $rejectedMSG;
+
+        $mailMessage = (new MailMessage)
+            ->subject('Update On Your Application')
+            ->line($message);
+
+        //TODO: write the api route
+        // if ($this->accept) {
+        //     $mailMessage->action('Your Profile', route(''));
+        // }
+
+        $mailMessage->line('Thank you for using our application!');
+
+        return $mailMessage;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
