6af9282e456dc79764414fa664c2780d09cc1549
Abrar-Abdulwahed <abrar.abdulwahed@gmail.com>
2024-01-28 14:44:34 +0300

    feat: prevent user from cancel accepted/refused reservation

---
+++ app/Http/Requests/Patient/ReservationDestroyRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\Patient;
+
+use App\Enums\OrderStatus;
+use App\Models\ProviderService;
+use Illuminate\Validation\Rule;
+use App\Http\Requests\BaseRequest;
+use App\Models\ServiceAvailability;
+use Illuminate\Validation\Validator;
+
+class ReservationDestroyRequest extends BaseRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [];
+    }
+
+    public function after(): array
+    {
+        return [
+            function (Validator $validator) {
+                $instance = $this->reservation;
+                if ($instance->status !== OrderStatus::PENDING->value) {
+                    $validator->errors()->add(
+                        'status',
+                        'You can\'t cancel this reservation, because it\'s ' . $instance->getStatusLabel($instance->status) . ' by provider!'
+                    );
+                }
+            }
+        ];
+    }
+}
