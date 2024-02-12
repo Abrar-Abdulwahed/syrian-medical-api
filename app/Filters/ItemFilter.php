b55a15d753d85a0c3ec970f58e3ebd46f47aebb3
Abrar-Abdulwahed <abrar.abdulwahed@gmail.com>
2024-02-13 01:27:07 +0300

    refactor: enhance query with relation

--- app/Filters/ItemFilter.php
+++ app/Filters/ItemFilter.php
@@ -10,20 +10,15 @@ class ItemFilter extends ApplyFilter
     public function category($value)
     {
         if ($this->query->getModel() instanceof ProviderService) {
-            return $this->query->whereHas('service', function ($query) use ($value) {
-                $query->where('category_id', $value);
-            });
+            return $this->query->whereRelation('service', 'category_id',  $value);
         }
-        return null;
     }
 
     public function title($value)
     {
         if ($this->query->getModel() instanceof ProviderService) {
-            return $this->query->whereHas('service', function ($query) use ($value) {
-                $query->where('title_ar', 'LIKE', "%{$value}%")
-                    ->orWhere('title_en', 'LIKE', "%{$value}%");
-            });
+            return $this->query->whereRelation('service', 'title_ar',  'LIKE', "%{$value}%")
+                ->orWhereRelation('service', 'title_en',  'LIKE', "%{$value}%");
         } else {
             return $this->query->where('title_ar', 'LIKE', '%' . $value . '%')->orWhere('title_en', 'LIKE', '%' . $value . '%');
         }
